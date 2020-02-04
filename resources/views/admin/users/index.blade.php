ab9809fe808533fee552f8bf10bb82adf31ba064
FREDERIC <kannif@ymail.com>
2020-02-04 07:00:02 +0400

    table bootstrap + btn edit et delete

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,11 +6,34 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Liste des utilisateurs</div>
-@foreach ($users as $user )
-<p>{{$user-> name}}-{{$user-> email}}</p>
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      
+                      @foreach ($users as $user )
+                     
+                      <tr>
+                        <th scope="row">{{$user-> id}}</th>
+                        <td>{{$user-> name}}</td>
+                        <td>{{$user-> email}}</td>
+                        <td>
+                        <a href="{{route('admin.users.edit', $user-> id)}}"><button class="btn btn-primary">éditer</button></a>
+                            <a href="{{route('admin.users.destroy', $user-> id)}}"><button class="btn btn-danger">supprimer</button></a>
+                        </td>
+                      </tr>
+                          
+                      @endforeach
+                    </tbody>
+                  </table>
 
-    
-@endforeach
+
               
             </div>
         </div>
